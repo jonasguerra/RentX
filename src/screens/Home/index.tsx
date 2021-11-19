5e7c361efa1854ff366d8b344fe62fc8c9c9d8ad
Jonas Guerra <guerra.jonas@hotmail.com>
2021-11-19 10:11:16 -0300

    Types

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 import Logo from '../../assets/logo.svg';
-import { Container, Header } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  TotalCars,
+} from './styles';
 
 export function Home() {
   //com o translucent, o header começa "embaixo" da status bar
   return (
     <Container>
-      <StatusBar barStyle="light-content" />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Header>
-        <Logo width={120} height={120} />
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
       </Header>
     </Container>
   );
